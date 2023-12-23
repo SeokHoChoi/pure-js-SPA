ea91dfa753d320ec5fb414776b8826ac77f404d6
seokhochoi <seokho9473@gmail.com>
2023-12-23 16:11:32 +0900

    feat: Component interface 초기 메서드 생성 및 App 컴포넌트 렌더링 테스트

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 import Component from './core/component.js';
 import TechBlogPage from './pages/tech-blog-page.js';
 
-export default class App extends Component {}
+export default class App extends Component {
+  _template() {
+    return '<main data-component="app">test</main>';
+  }
+
+  _componentDidMount() {
+    this.$target.innerHTML = this._template();
+    const $app = this.$target.querySelector('[data-component="app"]');
+    console.log($app, 'test');
+  }
+}
--- src/core/component.js
+++ src/core/component.js
@@ -1,4 +1,28 @@
 /**
  * 구현에 필요한 코어 interface
  */
-export default class Component {}
+export default class Component {
+  #$target;
+
+  constructor($target) {
+    this.#$target = $target;
+    this._componentDidMount();
+  }
+
+  _template() {
+    return '';
+  }
+
+  _componentDidMount() {}
+
+  get $target() {
+    return this.#$target;
+  }
+
+  /**
+   * @param {HTMLElement} $target
+   */
+  set $target($target) {
+    this.#$target = $target;
+  }
+}
