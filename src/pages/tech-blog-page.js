2e6222adbdb49979371f8788b7e95e024ea3c0b6
seokhochoi <seokho9473@gmail.com>
2023-12-31 23:07:28 +0900

    feat: tech-blog-page.js 파일에 fetch 통신 코드와 HTML 템플릿 추가

--- src/pages/tech-blog-page.js
+++ src/pages/tech-blog-page.js
@@ -1,19 +1,38 @@
 import Component from '../core/component.js';
-import TechDetailPage from './tech-detail-page.js';
 
 export default class TechBlogPage extends Component {
-  _template() {
-    return `
-        <div data-component="tech-blog-page">
-          TechBlogPage
-        </div>
-      `;
+  _setupInitialState() {
+    fetch('src/data/mockDataOfTech.json')
+      .then((response) => response.json())
+      .then((data) => {
+        this._$state = data;
+        // 상태 변경을 반영하기 위해 실행
+        this._render();
+      });
   }
 
-  _componentDidUpdate() {
-    const $techBlogPage = this._$target.querySelector(
-      '[data-component="tech-blog-page"]'
-    );
-    new TechDetailPage($techBlogPage);
+  _template() {
+    const { techs } = this._$state;
+
+    return `
+      <div >
+        ${techs
+          .map((tech) => {
+            return `
+              <div key=${tech.id}>
+                <img src=${tech.imageURL} alt="${tech.alt}" />
+                <div>
+                  <h2>${tech.title}</h2>
+                  <p>${tech.desc}</p>
+                  <time datetime=${tech.date}>${tech.date.replace(
+              /-/g,
+              '. '
+            )}</time>
+                </div>
+              </div>`;
+          })
+          .join('')}
+      </div>
+    `;
   }
 }
