6a3138356c568468e722a9e33a70fb7fe00eebe2
SeokHoChoi <seokho9473@gmail.com>
2024-01-11 02:59:32 +0900

    feat: header element 타입수정

--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,15 @@ export default class App extends Component<HTMLDivElement> {
 
   _componentDidUpdate() {
     const $header = this._$target.querySelector('[data-component="header"]');
-    if ($header instanceof HTMLElement) {
+    if ($header instanceof HTMLHeadElement) {
       new Header($header);
-    }
+    } 
+
     const $app = this._$target.querySelector('[data-component="app"]');
     const pages = createPages($app);
 
+    // <main> 태그는 별도의 인터페이스가 없어서 HTMLElement를 따릅니다.
+    // 태그의 타입이 인터페이스로 명확하게 정의되지 않았다면 해당 태그는 기본적으로 HTMLElement로 간주됩니다.
     if ($app instanceof HTMLElement) {
       const router = new Router($app);
       router
