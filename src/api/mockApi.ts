0ab60211c1129e616b9ae2bb854a939d3e529a1c
seokhochoi <seokho9473@gmail.com>
2024-01-12 23:12:42 +0900

    feat: mockApi 타입 적용

--- src/api/mockApi.ts
+++ src/api/mockApi.ts
@@ -1,8 +1,20 @@
-const parseResponse = async (response: any) => {
+interface IResponse<T> {
+  status: number;
+  data: T | undefined;
+}
+
+interface IRequestParams {
+  method?: string;
+  url: string;
+  headers?: Record<string, string>;
+  body?: Record<string, unknown>;
+}
+
+const parseResponse = async <T>(response: Response): Promise<IResponse<T>> => {
   const { status } = response;
   let data;
   // 클라이언트의 요청은 정상적이나 컨텐츠를 제공하지 않는경우(HTTP Response body가 존재하지 않습니다)
-  if (status !== 204) {
+  if (status !== 204 && status >= 200 && status < 300) {
     data = await response.json();
   }
 
@@ -12,12 +24,15 @@ const parseResponse = async (response: any) => {
   };
 };
 
-const request = async (params: any) => {
-  const { method = 'GET', url, headers = {}, body } = params;
-
-  const config: any = {
+const request = async ({
+  method = 'GET',
+  url,
+  headers = {},
+  body,
+}: IRequestParams): Promise<IResponse<unknown>> => {
+  const config: RequestInit = {
     method,
-    headers: new window.Headers(headers),
+    headers: new Headers(headers),
   };
 
   if (body) {
@@ -29,7 +44,10 @@ const request = async (params: any) => {
   return parseResponse(response);
 };
 
-export const get = async (url:string, headers: any) => {
+const get = async (
+  url: string,
+  headers: Record<string, string>
+): Promise<unknown> => {
   const response = await request({
     url,
     headers,
